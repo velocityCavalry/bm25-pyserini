f16be002d5250103cbe299177d7bce8cd374c74c
velocityCalvary <xyu530@uw.edu>
2021-05-21 07:13:21 -0700

    add functions to read xor data file

--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,32 @@
+RESET_OPT=0
+while getopts ":l:" opt; do
+    case ${opt} in
+        l ) LANGUAGE=$OPTARG;;
+    esac
+done
 
+python -m wikiextractor.WikiExtractor /gscratch/cse/datasets/wikidump/${LANGUAGE}wiki-20190201-pages-articles-multistream.xml.bz2 \
+--json -o /gscratch/cse/datasets/wikidump/${LANGUAGE}-wiki-dump -b 100M;
+echo "finish dumping";
+
+
+cd /gscratch/cse/xyu530/bm25_pyserini/;
+mkdir /gscratch/cse/datasets/wikidump/${LANGUAGE}-wiki-edit;
 # change jsonl files' field from text to contents
-python edit_json_file.py --input-dir ../ja-wiki-dump/AA --output-dir ../ja-wiki-edit
+python edit_json_file.py --input-dir /gscratch/cse/datasets/wikidump/${LANGUAGE}-wiki-dump/AA --output-dir /gscratch/cse/datasets/wikidump/${LANGUAGE}-wiki-edit;
+cd /gscratch/cse/datasets/wikidump/${LANGUAGE}-wiki-edit/;
+for f in wiki_*; do
+  mv -- "$f" "$f.jsonl";
+done
+echo "finish trimming the paragraphs";
 
 #index the jsonl files
-python -m pyserini.index -collection JsonCollection -generator DefaultLuceneDocumentGenerator -threads 8  \
--input /gscratch/cse/xyu530/ja-wiki-edit -index indexes/jawiki-collection-jsonl -storePositions \
- -storeDocvectors -storeRaw
+cd /gscratch/scrubbed/xyu530/pyserini/;
+python -m pyserini.index -collection JsonCollection -language ${LANGUAGE} -generator DefaultLuceneDocumentGenerator -threads 8  \
+-input /gscratch/cse/datasets/wikidump/${LANGUAGE}-wiki-edit -index indexes/${LANGUAGE}wiki-collection-jsonl -storePositions \
+ -storeDocvectors -storeRaw;
+echo "finish indexing the wiki";
 
-export PYTHONPATH=$(pwd) # in pyserini directory
-python /gscratch/cse/xyu530/bm25_pyserini/search_index.py --index-path indexes/jawiki-collection-jsonl --query-path
-/gscratch/cse/xyu530/mkqa.jsonl.gz --output work_dir/ja-test-output.jsonl --lang ja
+export PYTHONPATH=$(pwd); # in pyserini directory
+python /gscratch/cse/xyu530/bm25_pyserini/search_index.py --index-path indexes/${LANGUAGE}wiki-collection-jsonl --query-path /gscratch/cse/xyu530/mkqa.jsonl.gz --output work_dir/${LANGUAGE}-output.json --lang ${LANGUAGE};
+echo "finish all"
--- search_index.py
+++ search_index.py
@@ -44,6 +44,23 @@ def return_q2id_answer(file, lang):
     return q2idan
 
 
+def return_q2id_answer_xor(file, lang):
+    q2idan = dict()
+    with open(file, 'rb') as f:
+        for line in f:
+            line = line.decode('utf-8')
+            example = json.loads(line)
+            if example["lang"] != lang:
+                continue
+            question = example["question"]
+            id = example["id"]
+            answers = [{"text": "", "answer_start": -1}]
+            if question in q2idan:
+                print(f'prev question: {question}, prev id:{q2idan[question][0]}, curr id: {id}\n')
+            q2idan[question] = (id, answers)
+    return q2idan
+
+
 def search_indexes(searcher, query, id, answers):
     # index_reader = IndexReader(index_path)
     hits = searcher.search(query, k=10)
@@ -63,6 +80,7 @@ def main():
     parser.add_argument('--index-path', required=True, type=str, help='the path to the wikipedia index')
     parser.add_argument('--query-path', required=True, type=str, help='the path to the mkqa question file')
     parser.add_argument('--output', required=True, type=str, help='the path to the output jsonl file')
+    parser.add_argument('--xor', action='store_true', help='index xor question files and output squad formatted data')
     parser.add_argument('--lang', required=True, help='the language')
     parser.add_argument('--k1', default=0.82, help='the k1 in bm25')
     parser.add_argument('--b', default=0.68, help='the b in bm25')
@@ -71,7 +89,6 @@ def main():
     # if args.lang == 'zh':
     #     args.lang = 'zh_cn'
 
-    q2id = return_q2id_answer(args.query_path, args.lang)
     searcher = SimpleSearcher(args.index_path)
     if args.lang == 'ar':
         analyzer = analysis.get_lucene_analyzer(name="Arabic")
@@ -124,23 +141,46 @@ def main():
     elif args.lang == 'zh_cn' or args.lang == 'zh_tw' or args.lang == 'ja' or args.lang == 'ko':
         analyzer = analysis.get_lucene_analyzer(name="CJK")
         searcher.set_analyzer(analyzer)
+    elif args.lang == 'en':
+        analyzer = analysis.get_lucene_analyzer(name="English2")
+        searcher.set_analyzer(analyzer)
+    elif args.lang == 'bn':
+        analyzer = analysis.get_lucene_analyzer(name="Bengali")
+        searcher.set_analyzer(analyzer)
 
     searcher.set_bm25(args.k1, args.b)
 
+    if args.xor:
+        q2id = return_q2id_answer_xor(args.query_path, args.lang)
+        with open(args.output, 'w+', encoding='utf-8') as fw:
+            squad_formatted_content = dict()
+            squad_formatted_content["version"] = f"xor-{args.lang}-bm25"
+            data = []
+            for i, query in enumerate(tqdm(q2id)):
+                data_element = dict()
+                data_element['title'] = str(i)  # dummy
+                id, answers = q2id[query]
+                data_element['paragraphs'] = search_indexes(searcher, query, id, answers)
+                data.append(data_element)
+                squad_formatted_content["data"] = data
+            json.dump(squad_formatted_content, fw, ensure_ascii=False)
+            fw.flush()
 
-    with open(args.output, 'w+', encoding='utf-8') as fw:
-        squad_formatted_content = dict()
-        squad_formatted_content["version"] = f"{args.lang}-bm25"
-        data = []
-        for i, query in enumerate(tqdm(q2id)):
-            data_element = dict()
-            data_element['title'] = str(i)  # dummy
-            id, answers = q2id[query]
-            data_element['paragraphs'] = search_indexes(searcher, query, id, answers)
-            data.append(data_element)
-        squad_formatted_content["data"] = data
-        json.dump(squad_formatted_content, fw, ensure_ascii=False)
-        fw.flush()
+    else:
+        q2id = return_q2id_answer(args.query_path, args.lang)
+        with open(args.output, 'w+', encoding='utf-8') as fw:
+            squad_formatted_content = dict()
+            squad_formatted_content["version"] = f"{args.lang}-bm25"
+            data = []
+            for i, query in enumerate(tqdm(q2id)):
+                data_element = dict()
+                data_element['title'] = str(i)  # dummy
+                id, answers = q2id[query]
+                data_element['paragraphs'] = search_indexes(searcher, query, id, answers)
+                data.append(data_element)
+            squad_formatted_content["data"] = data
+            json.dump(squad_formatted_content, fw, ensure_ascii=False)
+            fw.flush()
 
 
 if __name__ == '__main__':
